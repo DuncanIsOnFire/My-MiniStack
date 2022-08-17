bad38f9a4c8b8eab3cc35b6f1dbe313a91eecfe8
DuncanIsOnFire <duncanfair@gmail.com>
2022-08-16 20:25:55 -0400

    adding simple latency monitoring, updating image name, adding scraping

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # My-MiniStack
+
+Currently Implemented:
+
+Future: Argo CD, NGINX
+Chaos Test
+
+Read: https://prometheus.io/docs/prometheus/latest/storage/, terraform,
--- src/app.py
+++ src/app.py
@@ -1,19 +1,26 @@
 import time
+import random
 import prometheus_client
 
 from flask import Response, Flask
 
 from prometheus_client.core import CollectorRegistry
 from prometheus_client import Counter
+from prometheus_client import Histogram
 
 site = Flask( __name__ )
 
 metrics = []
 metrics.append( Counter( 'python_request_count', 'Total processed requests' ) )
+metrics.append( Histogram( 'request_latency_seconds', 'Number of seconds to process a request.' ) )
 
 @site.route('/')
 def hello_world():
     metrics[0].inc()
+    start_time = time.time()
+    time.sleep( random.random( ) )
+    end_time = time.time() - start_time
+    metrics[1].observe( end_time )
     return 'Hello World'
 
 @site.route("/metrics")
@@ -22,7 +29,7 @@ def app_metrics():
     for v in metrics:
         resp.append( prometheus_client.generate_latest(v) )
     return Response( resp, mimetype = "text/plain")
-    return "<br>".join( resp )
+#   return "<br>".join( resp )
 
 if __name__ == '__main__':
     site.run()
